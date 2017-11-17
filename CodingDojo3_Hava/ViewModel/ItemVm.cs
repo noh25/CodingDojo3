f3a18542103ccf3d68f1e3f937d37d18749964c9
nikolas08 <nikolas.hava@outlook.com>
2017-11-17 21:19:54 +0100

    included reference to dll

---
+++ CodingDojo3_Hava/ViewModel/ItemVm.cs
@@ -0,0 +1,50 @@
+﻿using GalaSoft.MvvmLight;
+using Shared.BaseModels;
+using Shared.Models;
+
+namespace CodingDojo3_Hava.ViewModel
+{
+    public class ItemVm : ViewModelBase
+
+    { 
+        private ItemBase baseItem;
+
+        public int Id
+        {
+            get { return this.Id; }
+            //set;
+        }
+        public string Description
+        {
+            get { return this.Description; }
+            set { this.Description = value; RaisePropertyChanged(); }
+        }
+        public string Name
+        {
+            get { return this.Name; }
+            set { this.Name = value; RaisePropertyChanged(); }
+        }
+        public string Room
+        {
+            get { return this.Room; }
+            set { this.Room = value; RaisePropertyChanged(); }
+        }
+        public int PosX
+        {
+            get { return this.PosX; }
+            set { this.PosX = value; RaisePropertyChanged(); }
+        }
+        public int PosY
+        {
+            get { return this.PosY; }
+            set { this.PosY = value; RaisePropertyChanged(); }
+        }
+        public string ValueType
+        {
+            get { return myVar; }
+            
+        }
+
+
+    }
+}
