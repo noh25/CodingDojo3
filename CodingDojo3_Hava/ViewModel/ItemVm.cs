5a2050e6347b6d326c20c762edac47e231fe7f8b
nikolas08 <nikolas.hava@outlook.com>
2017-11-19 14:40:00 +0100

    problem in ViewModelLocator encountered

--- CodingDojo3_Hava/ViewModel/ItemVm.cs
+++ CodingDojo3_Hava/ViewModel/ItemVm.cs
@@ -1,6 +1,8 @@
 ﻿using GalaSoft.MvvmLight;
 using Shared.BaseModels;
+using Shared.Interfaces;
 using Shared.Models;
+using System;
 
 namespace CodingDojo3_Hava.ViewModel
 {
@@ -41,10 +43,70 @@ namespace CodingDojo3_Hava.ViewModel
         }
         public string ValueType
         {
-            get { return myVar; }
-            
+            get
+            {
+                if (baseItem is ISensor) return (baseItem as BaseSensor).SensorValueType.Name;
+                else if (baseItem is IActuator) return (baseItem as BaseActuator).ActuatorValueType.Name;
+                else throw new NotImplementedException();
+            }
+        }
+        public Type ItemType
+        {
+            get
+            {
+                if (baseItem is ISensor) return typeof(ISensor);
+                else if (baseItem is IActuator) return typeof(IActuator);
+                else throw new NotImplementedException();
+            }
+        }
+        public string Mode
+        {
+            get
+            {
+                if (baseItem is ISensor) return (baseItem as BaseSensor).SensorMode.ToString();
+                else if (baseItem is IActuator) return (baseItem as BaseActuator).ActuatorMode.ToString();
+                else return null;
+            }
+            set
+            {
+                if (baseItem is ISensor)
+                    (baseItem as BaseSensor).SensorMode = (SensorModeType)Enum.Parse(typeof(SensorModeType), value, false);
+                else if (baseItem is IActuator)
+                    (baseItem as BaseActuator).ActuatorMode = (ModeType)Enum.Parse(typeof(ModeType), value, false);
+
+                RaisePropertyChanged();
+            }
+        }
+        public object Value
+        {
+            get
+            {
+                if (baseItem is ISensor) return (baseItem as BaseSensor).SensorValue;
+                else if (baseItem is IActuator) return (baseItem as BaseActuator).ActuatorValue;
+                else throw new NotImplementedException();
+            }
+            set
+            {
+                if (baseItem is ISensor) (baseItem as BaseSensor).SensorValue = value;
+                else if (baseItem is IActuator) (baseItem as BaseActuator).ActuatorValue = value;
+                else throw new NotImplementedException();
+
+                RaisePropertyChanged();
+            }
+        }
+        public ItemVm(ISensor sensor)
+        {
+            baseItem = sensor as ItemBase;
         }
 
+        public ItemVm(IActuator actuator)
+        {
+            baseItem = actuator as ItemBase;
+        }
 
     }
+
+
+
+}
 }
